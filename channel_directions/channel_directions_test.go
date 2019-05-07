a6782c23ded72df564c051b29155f4e6dfeb89b9
ducnguyenlinh <duclinh0305@gmail.com>
2019-05-07 19:07:57 +0900

    Go by Example: Channel Directions

---
+++ channel_directions/channel_directions_test.go
@@ -0,0 +1,54 @@
+package main
+
+import "testing"
+
+func Test_ping(t *testing.T) {
+	type args struct {
+		pings chan<- string
+		msg   string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ping(tt.args.pings, tt.args.msg)
+		})
+	}
+}
+
+func Test_pong(t *testing.T) {
+	type args struct {
+		pings <-chan string
+		pongs chan<- string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			pong(tt.args.pings, tt.args.pongs)
+		})
+	}
+}
+
+func Test_main(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		{
+			name: "Test channel_directions",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			main()
+		})
+	}
+}
